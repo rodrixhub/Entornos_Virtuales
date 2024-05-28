9293614e687ae581bfdaae67a0a4cbc829cdf6ad
kruv15 <kevinurena82@gmail.com>
2024-05-28 18:03:40 -0400

    Se implemento el reproducir page

--- src/pages/ReproducirUsuario.jsx
+++ src/pages/ReproducirUsuario.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Layout, Typography, Button, Form, Radio, Modal, message } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Layout, Typography, Button, Form, Modal, message, Card, Checkbox } from 'antd';
+import { CheckCircleFilled, CloseCircleFilled } from '@ant-design/icons';
 import { UserLayout } from '../components/layouts/UserLayout';
 import { useParams } from 'react-router-dom';
 import { eduAPI } from '../services';
 
-// Componente Modal para resolver cuestionarios
-const ResolveQuestionnaireModal = ({ visible, onCancel, onSubmit, questions, form, selectedAnswers, handleAnswerChange, results }) => (
+const ResolveQuestionnaireModal = ({ visible, onCancel, onSubmit, question, form, handleAnswerChange }) => (
     <Modal title="Resolver Cuestionario" visible={visible} onCancel={onCancel} footer={null}>
         <Form
             form={form}
@@ -13,31 +13,24 @@ const ResolveQuestionnaireModal = ({ visible, onCancel, onSubmit, questions, for
             layout="vertical"
             onFinish={onSubmit}
         >
-            {questions.length > 0 ? (
-                questions.map((question, index) => (
-                    <div key={question._id} style={{ marginBottom: 16 }}>
-                        <Typography.Text>{question.questionText}</Typography.Text>
-                        <Form.Item
-                            name={`question_${question._id}`}
-                            rules={[{ required: true, message: 'Por favor seleccione una respuesta' }]}
-                        >
-                            <Radio.Group onChange={e => handleAnswerChange(question._id, e.target.value)}>
-                                {question.options.map((option, optionIndex) => (
-                                    <Radio key={`${question._id}-${optionIndex}`} value={option.text}>
-                                        {option.text}
-                                    </Radio>
-                                ))}
-                            </Radio.Group>
-                        </Form.Item>
-                        {results[question._id] !== undefined && (
-                            <Typography.Text type={results[question._id] ? "success" : "danger"}>
-                                {results[question._id] ? "Correcto" : "Incorrecto"}
-                            </Typography.Text>
-                        )}
-                    </div>
-                ))
+            {question ? (
+                <div key={question._id} style={{ marginBottom: 16 }}>
+                    <Typography.Text>{question.questionText}</Typography.Text>
+                    <Form.Item
+                        name={`question_${question._id}`}
+                        rules={[{ required: true, message: 'Por favor seleccione una respuesta' }]}
+                    >
+                        <Checkbox.Group onChange={checkedValues => handleAnswerChange(question._id, checkedValues)}>
+                            {question.options.map((option, optionIndex) => (
+                                <Checkbox key={`${question._id}-${optionIndex}`} value={option.text}>
+                                    {option.text}
+                                </Checkbox>
+                            ))}
+                        </Checkbox.Group>
+                    </Form.Item>
+                </div>
             ) : (
-                <Typography.Text>No hay preguntas disponibles para este video.</Typography.Text>
+                <Typography.Text>No hay preguntas disponibles para este tiempo.</Typography.Text>
             )}
             <Form.Item>
                 <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
@@ -52,13 +45,14 @@ export const ReproducirUsuario = () => {
     const { Content } = Layout;
     const { id } = useParams();
     const [video, setVideo] = useState(null);
-    //Modal de resolver cuestionario
-    const [isResolveModalVisible, setIsResolveModalVisible] = useState(false);
-    //constantes para el resolver cuestionario
-    const [form] = Form.useForm();
     const [questions, setQuestions] = useState([]);
+    //Funcion de pausar video
+    const videoRef = useRef(null);
+    // Estado del modal y la pregunta actual
+    const [isResolveModalVisible, setIsResolveModalVisible] = useState(false);
+    const [currentQuestion, setCurrentQuestion] = useState(null);
+    const [form] = Form.useForm();
     const [selectedAnswers, setSelectedAnswers] = useState({});
-    const [results, setResults] = useState({});
 
     const getVideo = async () => {
         try {
@@ -66,6 +60,8 @@ export const ReproducirUsuario = () => {
             if (data.success) {
                 setVideo(data.video);
                 setQuestions(data.video.preguntas || []);
+                console.log(data.video)
+                console.log(data.video.preguntas)
             }
         } catch (error) {
             console.log(error);
@@ -76,60 +72,75 @@ export const ReproducirUsuario = () => {
         getVideo();
     }, []);
 
-    // Función para mostrar el modal de resolver cuestionario
-    const showResolveModal = () => {
-        setIsResolveModalVisible(true);
-        setSelectedAnswers({});
-        setResults({});
+    // useEffect para añadir y remover el event listener del video
+    useEffect(() => {
+        const videoElement = videoRef.current;
+        if (videoElement) {
+            videoElement.addEventListener('timeupdate', handleTimeUpdate);
+            console.log('Event listener added');
+        }
+        return () => {
+            if (videoElement) {
+                videoElement.removeEventListener('timeupdate', handleTimeUpdate);
+                console.log('Event listener removed');
+            }
+        };
+    }, [video, questions]);
+    
+    // Función que pausa el video cuando se alcanza el tiempo de pausa
+    const handleTimeUpdate = () => {
+        const videoElement = videoRef.current;
+        if (videoElement) {
+            console.log(`Current time: ${videoElement.currentTime}`);
+            questions.forEach(question => {
+                const questionTime = parseFloat(question.time);
+                if (videoElement.currentTime >= questionTime && videoElement.currentTime < questionTime + 0.3) {
+                    videoElement.pause();
+                    console.log(`Video paused at ${questionTime} seconds for question: ${question.questionText}`);
+                    setCurrentQuestion(question);
+                    setIsResolveModalVisible(true);
+                }
+            });
+        } else {
+            console.log('Video element is not defined in handleTimeUpdate');
+        }
     };
+
+    // Función para manejar el cambio de respuesta seleccionada
+    const handleAnswerChange = (questionId, checkedValues) => {
+        setSelectedAnswers(prev => ({ ...prev, [questionId]: checkedValues }));
+    };
+    
+    // Función para manejar el envío del formulario de resolver cuestionario
+    const handleResolveFormSubmit = async () => {
+        const question = currentQuestion;
+        if (question) {
+            if (question.options && question.options.length > 0) {
+                const correctAnswers = question.options.filter(option => option.isCorrect).map(option => option.text);
+                const selectedAnswer = selectedAnswers[question._id] || [];
+                const isCorrect = correctAnswers.length === selectedAnswer.length && correctAnswers.every(answer => selectedAnswer.includes(answer));
+                message.open({
+                    content: (
+                        <span>
+                            {isCorrect ? <CheckCircleFilled style={{ color: 'green', marginRight: '8px'}} /> : <CloseCircleFilled style={{ color: 'red', marginRight: '8px' }} />}
+                            {isCorrect ? "Correcto" : "Incorrecto"}
+                        </span>
+                    ),
+                    duration: 2,
+                });
+            }
+        }
+        setIsResolveModalVisible(false);
+        setCurrentQuestion(null);
+        form.resetFields();
+    };    
+
     // Función para cerrar el modal de resolver cuestionario y resetear el formulario
     const handleResolveCancel = () => {
         setIsResolveModalVisible(false);
+        setCurrentQuestion(null);
         form.resetFields();
-        setSelectedAnswers({});
-        setResults({});
     };
-    // Función para manejar el envío del formulario de resolver cuestionario
-    const handleResolveFormSubmit = async () => {
-        const results = {};
-        questions.forEach(question => {
-            if (question.options && question.options.length > 0) {
-                const correctAnswer = question.options.find(option => option.isCorrect === true);
-                if (correctAnswer) {
-                    const selectedAnswer = selectedAnswers[question._id];
-                    results[question._id] = selectedAnswer === correctAnswer.text;
-                } else {
-                    results[question._id] = false; // No hay respuesta correcta configurada
-                }
-            } else {
-                results[question._id] = false; // No hay opciones configuradas
-            }
-        });
-        setResults(results);
-        message.success("Cuestionario resuelto exitosamente");
-    };
-    // Función para manejar el cambio de respuesta seleccionada
-    const handleAnswerChange = (questionId, answerText) => {
-        setSelectedAnswers(prev => ({ ...prev, [questionId]: answerText }));
-    };
-    /*const showResolveModal = () => {
-        setIsResolveModalVisible(true);
-    };
-
-    const handleResolveCancel = () => {
-        setIsResolveModalVisible(false);
-    };
-
-    const handleResolveFormSubmit = async (values) => {
-        try {
-            message.success("Cuestionario resuelto exitosamente");
-        } catch (error) {
-            message.error("Error en la solicitud al servidor");
-            console.error(error);
-        } finally {
-            setIsResolveModalVisible(false);
-        }
-    };*/
 
     return (
         <UserLayout>
@@ -145,28 +156,25 @@ export const ReproducirUsuario = () => {
                     padding: '20px'
                 }}
             >
-                {video ? (
-                    <div key={video._id} className="video-card" style={{ textAlign: 'center', width: '100%', maxWidth: '600px' }}>
-                        <video style={{ width: '100%', height: 'auto' }} controls>
-                            <source src={`http://localhost:8080/${video.videoPath}`} type="video/mp4" />
-                            Tu navegador no soporta el elemento de video.
-                        </video>
-                    </div>
-                ) : (
-                    <Typography.Text>Loading...</Typography.Text>
-                )}
-                <Button type="primary" onClick={showResolveModal} style={{ marginTop: '10px' }}>
-                    Resolver Cuestionario
-                </Button>
+                <div style={{ display: 'flex', backgroundColor: '#e0e0e0', padding: '20px', borderRadius: '10px', maxWidth: '620px', width: '100%' }}>
+                    {video ? (
+                        <Card className="video-section" style={{ flex: 3, backgroundColor: '#fff', borderRadius: '10px' }}>
+                            <video ref={videoRef} style={{ width: '100%', height: 'auto' }} controls>
+                                <source src={`http://localhost:8080/${video.videoPath}`} type="video/mp4" />
+                                Tu navegador no soporta el elemento de video.
+                            </video>
+                        </Card>
+                    ) : (
+                        <Typography.Text>Loading...</Typography.Text>
+                    )}
+                </div>
                 <ResolveQuestionnaireModal
                     visible={isResolveModalVisible}
                     onCancel={handleResolveCancel}
                     onSubmit={handleResolveFormSubmit}
-                    questions={questions}
+                    question={currentQuestion}
                     form={form}
-                    selectedAnswers={selectedAnswers}
                     handleAnswerChange={handleAnswerChange}
-                    results={results}
                 />
             </Content>
         </UserLayout>
